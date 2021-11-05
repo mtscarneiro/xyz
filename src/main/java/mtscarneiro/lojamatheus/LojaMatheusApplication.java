8ee52d27f0b1be776193ae2bc7d9ff5600d3c007
mtscarneiro <dev.mtscarneiro@gmail.com>
2021-11-04 21:03:45 -0400

    project started

---
+++ src/main/java/mtscarneiro/lojamatheus/LojaMatheusApplication.java
@@ -0,0 +1,13 @@
+package mtscarneiro.lojamatheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaMatheusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LojaMatheusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mtscarneiro/lojamatheus/LojaMatheusApplicationTests.java
@@ -0,0 +1,13 @@
+package mtscarneiro.lojamatheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaMatheusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
