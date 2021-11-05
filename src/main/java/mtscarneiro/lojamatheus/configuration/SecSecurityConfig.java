8260356945cd0cc1d46d0f3602f1b0c99727c9de
mtscarneiro <dev.mtscarneiro@gmail.com>
2021-11-05 00:08:24 -0400

    messing with spring security once again, might finish it later

---
+++ src/main/java/mtscarneiro/lojamatheus/configuration/SecSecurityConfig.java
@@ -0,0 +1,12 @@
+package mtscarneiro.lojamatheus.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource({ "classpath:webSecurityConfig.xml" })
+public class SecSecurityConfig {
+    public SecSecurityConfig() {
+        super();
+    }
+}
